a60f36943d46297913d8b001c771a57b0addf6a6
shree0007 <shree.bhusal07@gmail.com>
2023-08-18 12:08:21 +0300

    shopping app first commit

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+
+
+const Test = () => {
+    return (
+        <div>
+
+            <Button variant='primary'>Example Button</Button>
+
+        </div>
+    );
+};
+
+export default Test;
