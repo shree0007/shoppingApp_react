b2ecf3c67c1ff8704c811a5ea2038fea608a22f5
shree0007 <shree.bhusal07@gmail.com>
2023-08-21 16:26:03 +0300

    styled homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
+import image from './homepage.jpg';
+import Button from 'react-bootstrap/Button';
 
 
 const Homepage = () => {
     return (
         <div>
             <NavBar />
-            <h1>Home Page</h1>
-            <p>This is a home page</p>
+            <div style={{ height: '100vh', backgroundImage: `url(${image}`, backgroundPosition: 'center center', backgroundSize: 'cover center', backgroundRepeat: 'no-repeat', padding: '3rem' }}>
+                <h1 style={{ color: 'brown' }}>Welcome to SHreeShopHub</h1>
+                <p>- Empowering Your Shopping Experience.</p>
+
+                <Link to={'/products'}><Button variant='primary'>Browse all products</Button></Link>
+            </div>
+
+
         </div>
     );
 };
